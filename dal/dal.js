60ce5657f698d1b91f2a9f2434c9f9076e97be04
janicekuo <47550853+janice880624@users.noreply.github.com>
2022-09-24 17:22:05 +0800

    0924

--- dal/dal.js
+++ dal/dal.js
@@ -5,6 +5,35 @@ var x;
 var myFirebase;
 var y;
 
+//  display use var
+var date;
+var time;
+var status_f;
+var myFirebase2;
+var val_1;
+var val_2;
+var val_3;
+
+date = '';
+myFirebase2 = new Firebase('https://dal-smart-manufacturing-default-rtdb.firebaseio.com/door');
+myFirebase2.limitToLast(1).on('child_added', function (snapshot) {
+    val_1 = snapshot.val().date;
+    document.getElementById('firebase-date-show').innerHTML = val_1;
+    date = val_1;
+});
+myFirebase2.limitToLast(1).on('child_added', function (snapshot) {
+    val_2 = snapshot.val().time;
+    document.getElementById('firebase-time-show').innerHTML = val_2;
+    time = val_2;
+});
+myFirebase2.limitToLast(1).on('child_added', function (snapshot) {
+    val_3 = snapshot.val().status;
+    document.getElementById('firebase-status-show').innerHTML = val_3;
+    status_f = val_3;
+});
+
+
+
 function get_time(t) {
     var varTime = new Date(),
         varHours = varTime.getHours(),
