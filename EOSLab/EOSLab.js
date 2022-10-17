eb177648520c48502e7f2ae3095a2dc547a9d3c1
janicekuo <47550853+janice880624@users.noreply.github.com>
2022-10-17 09:01:54 +0800

    1017

---
+++ EOSLab/EOSLab.js
@@ -0,0 +1,31 @@
+var date;
+var time;
+var status_f;
+var myFirebase2;
+var val_1;
+var val_2;
+var val_3;
+
+date = '';
+myFirebase2 = new Firebase('https://dal-smart-manufacturing-default-rtdb.firebaseio.com/ntnu');
+myFirebase2.limitToLast(1).on('child_added', function (snapshot) {
+    val_1 = snapshot.val().date;
+    document.getElementById('firebase-date-show').innerHTML = val_1;
+    date = val_1;
+});
+myFirebase2.limitToLast(1).on('child_added', function (snapshot) {
+    val_2 = snapshot.val().time;
+    document.getElementById('firebase-time-show').innerHTML = val_2;
+    time = val_2;
+});
+myFirebase2.limitToLast(1).on('child_added', function (snapshot) {
+    val_3 = snapshot.val().status;
+    document.getElementById('firebase-status-show').innerHTML = val_3;
+    status_f = val_3;
+});
+
+function adjustIframe(){
+    var ifm = document.getElementById("value_iframe");
+    ifm.height = document.documentElement.clientHeight;
+    ifm.width = document.documentElement.clientWidth;
+}
---
+++ EOSLab/data/data.js
@@ -0,0 +1,131 @@
+// DAL 門震動訊號
+var y_value = 0;
+var myFirebase;
+var val_1 = "";
+// var aseKey = "39398890";
+
+myFirebase = new Firebase('https://test-project-97787-default-rtdb.firebaseio.com/EOSLab');
+myFirebase.limitToLast(1).on('child_added', function (snapshot) {
+    val_1 = snapshot.val().valus;
+    console.log("val_1 => ", val_1);
+    y_value = Number(val_1);
+    // console.log("type of val_1 =>", typeof(val_1));
+
+    // var decrypt = CryptoJS.AES.decrypt(val_1, CryptoJS.enc.Utf8.parse(aseKey), {
+    //     mode: CryptoJS.mode.ECB,
+    //     padding: CryptoJS.pad.Pkcs7
+    // }).toString(CryptoJS.enc.Utf8);
+    // console.log("decrypt => ", decrypt);
+    
+    
+    // y_value = Number(decrypt);
+    // console.log(y_value);
+    // console.log(typeof(y_value));
+});
+
+
+$(document).ready(function() {  
+    var chart = {
+        type: 'spline',
+        animation: Highcharts.svg,
+        marginRight: 10,
+        events: {
+            load: function () {
+                var series = this.series[0];
+                setInterval(function () {
+                var x = (new Date()).getTime(), 
+                y = y_value;
+                series.addPoint([x, y], true, true);
+                }, 1000);
+            }
+        }
+    };
+
+    var title = {
+        text: '機台震動情況'   
+    };  
+
+    var credits={
+        enabled: false
+    };
+    
+
+    var xAxis = {
+        type: 'datetime',
+        tickPixelInterval: 150
+    };
+    var yAxis = {
+        title: {
+            text: 'Z 軸數值'
+        },
+        plotLines: [{
+            value: 0,
+            width: 1,
+            color: '#808080'
+        }]
+    };
+    var tooltip = {
+        formatter: function () {
+        return '<b>' + this.series.name + '</b><br/>' +
+            Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
+            Highcharts.numberFormat(this.y, 2);
+        }
+    };
+    var plotOptions = {
+        area: {
+            pointStart: 1940,
+            marker: {
+                enabled: false,
+                symbol: 'circle',
+                radius: 2,
+                states: {
+                hover: {
+                    enabled: true
+                }
+                }
+            }
+        }
+    };
+    var legend = {
+        enabled: false
+    };
+    var exporting = {
+        enabled: false
+    };
+    var series= [{
+        name: 'Random data',
+        data: (function () {
+            // generate an array of random data
+            var data = [],time = (new Date()).getTime(),i;
+            for (i = -19; i <= 0; i += 1) {
+                data.push({
+                    x: time + i * 2000,
+                    // y: Math.random(0, 1.5)
+                });
+            }
+            return data;
+        }())    
+    }];     
+        
+    var json = {};   
+    json.chart = chart; 
+    json.credits = credits; 
+    json.title = title;     
+    json.tooltip = tooltip;
+    json.xAxis = xAxis;
+    json.yAxis = yAxis; 
+    json.legend = legend;  
+    json.exporting = exporting;   
+    json.series = series;
+    json.plotOptions = plotOptions;
+    
+    
+    Highcharts.setOptions({
+        global: {
+            useUTC: false
+        }
+    });
+
+    $('#container').highcharts(json);
+    
+});
